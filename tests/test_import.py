90344c23e970be8935e7be24bd724543141fbbd7
Tohrusky <65994850+Tohrusky@users.noreply.github.com>
2024-01-18 02:10:12 +0800

    feat emnlp

---
+++ tests/test_import.py
@@ -0,0 +1,17 @@
+import aaai
+import cvpr
+import eccv
+import emnlp
+import iclr
+import icml
+import ijcai
+
+
+def test_import() -> None:
+    cvpr.kaiming()
+    eccv.kaiming()
+    aaai.kaiming()
+    ijcai.kaiming()
+    icml.kaiming()
+    iclr.kaiming()
+    emnlp.kaiming()
